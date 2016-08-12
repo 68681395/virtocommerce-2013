173e7346a7784125d83703254288aaef364aa73d
68681395 <happycoder@foxmail.com>
2016-08-12 14:45:42 +0800

    back up virtocommerce 2013 for memorial

---
+++ src/GlobalAssemblyInfo.cs
@@ -0,0 +1,23 @@
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// <copyright file="GlobalAssemblyInfo.cs" company="VirtoCommerce">
+//   Copyright © VirtoCommerce. All rights reserved.
+// </copyright>
+// <summary>
+//   Virto Commerce
+// </summary>
+// ----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+
+[assembly: AssemblyCompany("VirtoCommerce")]
+[assembly: AssemblyProduct("Virto Commerce 1.5")]
+[assembly: AssemblyCopyright("Copyright © VirtoCommerce 2011-2013")]
+[assembly: AssemblyFileVersion("1.5.619.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyInformationalVersion("1.5")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
